7af3ffde0ebdac938b46419305299180d6586859
= <=>
2020-07-01 12:54:37 +0700

    authors

--- src/components/Pages/Authors.js
+++ src/components/Pages/Authors.js
@@ -1,4 +1,14 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+import {withRouter, Link} from 'react-router-dom';
+
+import {withStyles} from '@material-ui/core/styles';
+
+
+import image from '../assets/img/banner/banner0.png';
+
+import Button from '@material-ui/core/Button';
 
 import AuthorItems from '../Common/AuthorItems';
 
@@ -7,12 +17,34 @@ const authors = [
     {}
 ];
 
+const styles = theme => ({
+    storeButton: {
+        width: '250px',
+        margin: '10px auto',
+        display: 'block',
+        fontSize: '14px',
+        marginBottom: '10px',
+        color: '#fff',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        padding: '10px',
+        backgroundColor: '#ff9800',
+        borderRadius: '25px'
+    },
+    buttonSize: {
+        fontSize: 'large',
+        padding: '0',
+        width: '100%'
+    },
+})
+
 class Authors extends Component {
 
     componentDidMount() {
         
     }
     render(){
+        const {classes} = this.props;
         return(
             <>
                 <div className="page-empty-banner col-lg-12 col-md-12 col-sm-12">
@@ -24,7 +56,7 @@ class Authors extends Component {
                     <div className="col-main">
                         <div className="container">
                             <div className={classes.storeButton}>
-                                <Button className={classes.buttonSize} onClick={this.toggleDrawer}>Tác giả</Button>
+                                <Button className={classes.buttonSize}>Tác giả</Button>
                                 
                             </div>
                         </div>
